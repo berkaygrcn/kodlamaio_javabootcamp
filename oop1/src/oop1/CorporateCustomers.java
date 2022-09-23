e5fa3809c27f715cf4918eb5a31daf2d7a65b9ef
berkaygrcn <102293169+berkaygrcn@users.noreply.github.com>
2022-09-23 19:36:33 +0300

    ders2ekleme

--- oop1/src/oop1/CorporateCustomers.java
+++ oop1/src/oop1/CorporateCustomers.java
@@ -1,5 +1,20 @@
 package oop1;
 
-public class CorporateCustomers {
+public class CorporateCustomers extends Customers{
+	
+	private String companyName;
+	private String taxNumber;
+	public String getCompanyName() {
+		return companyName;
+	}
+	public void setCompanyName(String companyName) {
+		this.companyName = companyName;
+	}
+	public String getTaxNumber() {
+		return taxNumber;
+	}
+	public void setTaxNumber(String taxNumber) {
+		this.taxNumber = taxNumber;
+	}
 
 }
--- oop1/src/oop1/Product.java
+++ oop1/src/oop1/Product.java
@@ -3,12 +3,44 @@ package oop1;
 //PascalCase
 public class Product {
 
-	String name;
+	private String name;
 	//camelCase
-	Double unitPrice;
-	Double discount;
-	String imageUrl;
-	int unitsInStock;
+	private Double unitPrice;
+	private Double discount;
+	private String imageUrl;
+	private int unitsInStock;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Double getUnitPrice() {
+		return unitPrice;
+	}
+	public void setUnitPrice(Double unitPrice) {
+		this.unitPrice = unitPrice;
+	}
+	public Double getDiscount() {
+		//discount=discount+5;
+		return discount;
+	}
+	public void setDiscount(Double discount) {
+		this.discount = discount;
+	}
+	public String getImageUrl() {
+		return imageUrl;
+	}
+	public void setImageUrl(String imageUrl) {
+		this.imageUrl = imageUrl;
+	}
+	public int getUnitsInStock() {
+		return unitsInStock;
+	}
+	public void setUnitsInStock(int unitsInStock) {
+		this.unitsInStock = unitsInStock;
+	}
 	
 	
 }
